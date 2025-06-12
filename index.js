beeff475bd47860335c2f3d4de33175e076bde27
ZosTer17 <abdulilprincipalekebabbaro.121@gmail.com>
2025-06-12 14:29:38 +0200

    new commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { token, clientId, devs } = require("./config.json");
 const fs = require("fs");
 const loadCommands = require("./utils/loadCommands");
 
-global.client = new Client({
+const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMembers,
@@ -12,6 +12,8 @@ global.client = new Client({
     ],
     partials: [Partials.Message]
 });
+
+module.exports = client;
 
 client.login(token);
 
